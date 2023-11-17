46118e1016f1ef1c2e5f703f066af7578c932268
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2023-11-17 11:26:50 +0000

    Tests project

---
+++ AddressesAPI.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AddressesAPI.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
